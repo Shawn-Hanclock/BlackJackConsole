9dafa0ba3cc1058a81da8e03fcd43ff0da376595
Shawn-Hanclock <90814768+Shawn-Hanclock@users.noreply.github.com>
2021-12-15 07:00:43 -0800

    Update Card.java

--- src/Card.java
+++ src/Card.java
@@ -1,21 +1,25 @@
 public class Card
 {
+    //initalization variables
     private Suit suit;
     private Value value;
     
+    //constructor
     public Card(Suit suit, Value value)
     {
         this.value = value;
         this.suit = suit;
-    }
+    }//end of constructor
 
+    //tostring
     public String toString()
     {
       return this.suit + "-" + this.value + "\n";
-    }
+    }//end of toString
 
+    //method that returns value as string instead of enum
     public String toValue()
     {
         return this.value + "";
-    }
-}
+    }//end of toValue method
+}//end of card class
